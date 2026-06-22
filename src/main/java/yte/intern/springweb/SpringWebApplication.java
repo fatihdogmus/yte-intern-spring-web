6eae672c2831cef11574a2eaa7287fb19c4cfd8f
fatihdogmus <fdogmus06@gmail.com>
2026-06-22 11:27:17 +0300

    spring boot yükseltildi

--- src/main/java/yte/intern/springweb/SpringWebApplication.java
+++ src/main/java/yte/intern/springweb/SpringWebApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SpringWebApplication {
 
-	public static void main(String[] args) {
+	void main(String[] args) {
 		SpringApplication.run(SpringWebApplication.class, args);
 	}
 
